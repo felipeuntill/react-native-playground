c303a29f63f7b2e1c3ce8614299a05cea05f468a
felipeuntill <felipeassuncao.net@gmail.com>
2017-05-09 19:12:54 -0300

    styles

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -2,7 +2,17 @@ import React from 'react';
 import { Text } from 'react-native';
 
 
-const Header = () => <Text>Albums</Text>;
+const Header = () => {
+  const { textStyle } = styles;
+  return <Text style={textStyle}>Albums</Text>;
+};
+
+const styles = {
+    textStyle: {
+      fontSize: 20,
+      color: 'red'
+    }
+};
 
 
 export default Header;

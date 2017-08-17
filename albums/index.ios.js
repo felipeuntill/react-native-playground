9660431f08acabdb053a9ad628d3fb1eef33daa1
felipeuntill <felipeassuncao.net@gmail.com>
2017-08-17 20:36:14 -0300

    state

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import { View, AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
   <View>
-    <Header />
+    <Header HeaderText="Albums" />
+    <AlbumList />
   </View>
 );
 

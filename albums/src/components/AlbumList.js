2ff8f643a61c7d6cc33c4c95038b52bc1402c0ab
felipeuntill <felipeassuncao.net@gmail.com>
2017-08-17 21:34:12 -0300

    activity indicador

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -15,7 +15,9 @@ class AlbumList extends Component {
   }
 
   renderAlbums() {
-    return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
+    return this.state.albums.length === 0 ?
+      <ActivityIndicator size="large" /> :
+      this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
   }
 
   render() {

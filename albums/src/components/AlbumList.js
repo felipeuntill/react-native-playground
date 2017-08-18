ecc5b8f8cf01df46738fd0a34e738fa205dee448
felipeuntill <felipeassuncao.net@gmail.com>
2017-08-17 21:28:21 -0300

    creating cards

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
 
@@ -13,10 +14,14 @@ class AlbumList extends Component {
       });
   }
 
+  renderAlbums() {
+    return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
+  }
+
   render() {
     return (
       <View>
-        <Text>Album List!!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }

9bf169afddfc7288b8994281195165cace9fd451
felipeuntill <felipeassuncao.net@gmail.com>
2017-08-21 21:38:43 -0300

    layout changes

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,21 +1,72 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = (props) => {
-  const album = props.album;
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
 
   return (
     <Card>
+
       <CardSection>
-        <Text>{album.title}</Text>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{ title }</Text>
+          <Text>{ artist }</Text>
+        </View>
       </CardSection>
+
       <CardSection>
-        <Text>{album.title}</Text>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }}
+        />
       </CardSection>
+
+      <CardSection>
+        <Button />
+      </CardSection>
+
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10
+  },
+  thumbnailStyle: {
+    width: 50,
+    height: 50
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
---
+++ albums/src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+
+const Button = (props) => {
+  return (
+    <Text>Click me!!!!</Text>
+  );
+};
+
+export default Button;

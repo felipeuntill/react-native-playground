c67a040f853cd2012660e8f151cbba20b47beb38
felipeuntill <felipeassuncao.net@gmail.com>
2017-08-21 22:55:50 -0300

    finishing the basic rest app

--- albums/src/components/Button.js
+++ albums/src/components/Button.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { TouchableOpacity, Text } from 'react-native';
 
 
-const Button = (props) => {
+const Button = ({ onPress, children }) => {
+
+  const { buttonStyle, textStyle } = styles;
+
   return (
-    <Text>Click me!!!!</Text>
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
+      <Text style={textStyle}>{children}</Text>
+    </TouchableOpacity>
   );
+};
+
+
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor:'#007aff'
+  }
 };
 
 export default Button;

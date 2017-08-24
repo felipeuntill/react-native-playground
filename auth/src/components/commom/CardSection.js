1aaa546d588d14262479e5d7caebec4cbd586809
felipeuntill <felipeassuncao.net@gmail.com>
2017-08-24 01:11:41 -0300

    eslint add

---
+++ auth/src/components/commom/CardSection.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CardSection = (props) => {
+  return (
+    <View style={styles.containerStyle}>
+      {props.children}
+    </View>
+  );
+};
+
+const styles = {
+  containerStyle: {
+    borderColor: '#ddd',
+    borderBottomWidth: 1,
+    paddingTop: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    position: 'relative',
+  }
+};
+
+export  { CardSection };
---
+++ auth/src/components/commom/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+        <Text style={textStyle}>{props.HeaderText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+    viewStyle: {
+      backgroundColor: '#F8F8F8',
+      justifyContent: 'center',
+      alignItems: 'center',
+      display: 'flex',
+      paddingTop: 20,
+      height: 60,
+      borderBottomColor: '#ddd',
+      borderBottomWidth: 1,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.1,
+      shadowRadius: 2,
+      elevation: 1,
+    },
+    textStyle: {
+      fontSize: 20
+    }
+};
+
+export  { Header };

a201f369731cae27fabe09d790f3627cdb2211b8
felipeuntill <felipeassuncao.net@gmail.com>
2017-08-24 01:16:25 -0300

    sobee

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text  } from 'react-native';
-
+import { Header } from './components/common/';
 
 class App extends Component {
 
   render() {
     return (
-      <View>
+      <View style={styles.rootViewStyle}>
+        <Header HeaderText="Login" />
         <Text>Working</Text>
       </View>
     );
   }
 
+}
+const styles = {
+  rootViewStyle : {
+    flex: 1
+  }
 }
 
 export default App;
